850742bf87fb12dd876e32901d5d88162f99a78b
Suzanne Millstein <smillst@cs.washington.edu>
2016-11-09 11:57:23 -0800

    Take a stab at installing Android. instructions from here: https://gist.githubusercontent.com/wenzhixin/43cf3ce909c24948c6e7/raw/089e9be0332e3e13a74a8688245f20a1c1fef9b6/ubuntu14.04-command-line-install-android-sdk

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,14 +3,35 @@ ROOT=$TRAVIS_BUILD_DIR/..
 
 # Fail the whole script if any command fails
 set -e
-export CHECKERFRAMEWORK=$ROOT/checker-framework
+
+wget http://dl.google.com/android/android-sdk_r24.2-linux.tgz
+tar -xvf android-sdk_r24.2-linux.tgz
+cd android-sdk-linux/tools
+# install all sdk packages
+./android update sdk --no-ui
+
+# set path
+vi ~/.zshrc << EOT
+
+export PATH=${PATH}:$HOME/sdk/android-sdk-linux/platform-tools:$HOME/sdk/android-sdk-linux/tools:$HOME/sdk/android-sdk-linux/build-tools/22.0.1/
+
+EOT
+
+source ~/.zshrc
+
+# adb
+sudo apt-get install libc6:i386 libstdc++6:i386
+# aapt
+sudo apt-get install zlib1g:i386
+
 ## Build Checker Framework
+export CHECKERFRAMEWORK=$ROOT/checker-framework
 (cd $ROOT && git clone --depth 9 https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
 (cd $CHECKERFRAMEWORK && ./.travis-build-without-test.sh)
 
 
-
+export ANDROID_HOME:$HOME/sdk/android-sdk-linux/
 ## Build
 ant jar
 
